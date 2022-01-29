985105543085d7c3440e5b235eedae015f918439
masudrana <masudranaacb@gmail.com>
2022-01-29 18:30:25 +0600

    array break conditional

--- break.js
+++ break.js
@@ -1,8 +1,8 @@
-var i= 0;
-while(i<=10){
-    console.log(i);
-    if(i==5){
+var numbers=[1,2,3,4,5,6,7,8,9,10];
+for(var i=0; i<numbers.length; i++){
+    var nunber=numbers[i]
+    console.log(nunber);
+    if(nunber==5){
         break;
     }
-    i++
 }
