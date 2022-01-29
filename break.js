8609d9aa4d330f24cd0a235271dd3523663889e5
masudrana <masudranaacb@gmail.com>
2022-01-29 18:41:08 +0600

    use is continue statment

--- break.js
+++ break.js
@@ -1,8 +1,8 @@
-var numbers=[1,2,3,4,5,6,7,8,9,10];
+var numbers=[10,12,3,14,5,6,7,8,9,10];
 for(var i=0; i<numbers.length; i++){
-    var nunber=numbers[i]
-    console.log(nunber);
-    if(nunber==5){
-        break;
+    var nunber=numbers[i];
+    if(nunber==8){
+        continue;
     }
+    console.log(nunber);
 }
